ffe531828fd2c01f7e0bfa4c3030d76314b7ff83
appuboss12 <appuforever638@gmail.com>
2025-04-22 07:02:54 +0530

    Update info.py

--- info.py
+++ info.py
@@ -93,7 +93,7 @@ IS_TUTORIAL = bool(environ.get('IS_TUTORIAL', False))
 # ============================
 # Channel & Group Links Configuration
 # ============================
-GRP_LNK = environ.get('GRP_LNK', 'https://t.me/SK_MOVIESZ')
+GRP_LNK = environ.get('GRP_LNK', 'https://t.me/+egrevu1YYPA3NDFl')
 CHNL_LNK = environ.get('CHNL_LNK', 'https://t.me/+gJ0zQ3OuiR84ODJl')
 OWNER_LNK = environ.get('OWNER_LNK', 'https://t.me/SK_MOVIESZ')
 DEENDAYAL_MOVIE_UPDATE_CHANNEL_LNK = environ.get('DEENDAYAL_MOVIE_UPDATE_CHANNEL_LNK', 'https://t.me/+gJ0zQ3OuiR84ODJl')
